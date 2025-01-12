93a14b1f29e8954bbd152152a37ce371e2acd475
Kunal Shrivastava <kunalshrivastava.iitr@gmail.com>
2025-01-12 14:26:31 +0100

    added suind commands

--- MAVProxy/modules/mavproxy_alias.py
+++ MAVProxy/modules/mavproxy_alias.py
@@ -10,18 +10,23 @@ class AliasModule(mp_module.MPModule):
         super(AliasModule, self).__init__(mpstate, "alias", "custom command aliases")
         self.add_command('ps', self.cmd_ps, "alias for 'param set'")
 
-    def cmd_ps(self, args):
-        if len(args) < 2:
-            print("Usage: ps PARAM_NAME VALUE")
-            return
-        param_name = args[0]
-        try:
-            value = float(args[1])  # Ensure the value is converted to a float
-        except ValueError:
-            print(f"Invalid value: {args[1]} is not a float")
-            return
+    def cmd_set_param_chksm(self, args):
+        param_name = "REC_PARAM_HSH"
+        value = 1
         self.master.param_set_send(param_name, value)
-        print(f"Set parameter {param_name} to {value}")
+        #print(f"Set parameter {param_name} to {value}")
+
+    def cmd_set_fw_chksm(self, args):
+        param_name = "REC_FW_HSH"
+        value = 1
+        self.master.param_set_send(param_name, value)
+        #print(f"Set parameter {param_name} to {value}")
+
+    def cmd_pair_gps(self, args):
+        param_name = "GPS_CACHE_UID"
+        value = 1
+        self.master.param_set_send(param_name, value)
+        #print(f"Set parameter {param_name} to {value}")
 
 def init(mpstate):
     return AliasModule(mpstate)
