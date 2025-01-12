fc055ba36390934120285a8937f29c3c0c69788f
Kunal Shrivastava <kunalshrivastava.iitr@gmail.com>
2025-01-12 12:42:58 +0100

    modified alias module to test something

--- MAVProxy/modules/mavproxy_alias.py
+++ MAVProxy/modules/mavproxy_alias.py
@@ -11,13 +11,17 @@ class AliasModule(mp_module.MPModule):
         self.add_command('ps', self.cmd_ps, "alias for 'param set'")
 
     def cmd_ps(self, args):
-        if len(args) < 2:
-            print("Usage: ps PARAM_NAME VALUE")
-            return
-        param_name = args[0]
-        value = args[1]
-        self.master.param_set_send(param_name, value)
-        print(f"Set parameter {param_name} to {value}")
+    if len(args) < 2:
+        print("Usage: ps PARAM_NAME VALUE")
+        return
+    param_name = args[0]
+    try:
+        value = float(args[1])  # Ensure the value is converted to a float
+    except ValueError:
+        print(f"Invalid value: {args[1]} is not a float")
+        return
+    self.master.param_set_send(param_name, value)
+    print(f"Set parameter {param_name} to {value}")
 
 def init(mpstate):
     return AliasModule(mpstate)
